4c5cc40e073eea461ce7153f9fa353a4ef580eb6
Barbara Soares <100702591+barbarasoaress@users.noreply.github.com>
2026-09-24 09:58:05 -0300

    Corrige faixas de classificação do herói

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 // Defina o nome e a quantidade de experiência do herói
-var nome = "Nome do Herói";
-var xp = 6000; // Substitua com a quantidade de experiência do seu herói
+const nome = "Nome do Herói";
+const xp = 6000;
 
-// Use uma estrutura de decisão para determinar o nível do herói com base na quantidade de experiência
-var nivel;
+// Determine o nível do herói com base na quantidade de experiência
+let nivel;
 
-if (xp < 1000) {
+if (xp <= 1000) {
   nivel = "Ferro";
-} else if (xp >= 1001 && xp <= 2000) {
+} else if (xp <= 2000) {
   nivel = "Bronze";
-} else if (xp >= 2001 && xp <= 5000) {
+} else if (xp <= 5000) {
   nivel = "Prata";
-} else if (xp >= 6001 && xp <= 7000) {
+} else if (xp <= 7000) {
   nivel = "Ouro";
-} else if (xp >= 7001 && xp <= 8000) {
+} else if (xp <= 8000) {
   nivel = "Platina";
-} else if (xp >= 8001 && xp <= 9000) {
+} else if (xp <= 9000) {
   nivel = "Ascendente";
-} else if (xp >= 9001 && xp <= 10000) {
+} else if (xp <= 10000) {
   nivel = "Imortal";
 } else {
   nivel = "Radiante";
 }
 
-// Exiba a mensagem com o nome e o nível do herói
-console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
